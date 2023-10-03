2914b87abc7b4eed01916af103153a8a30cde316
leander006 <leanderdsilva06@gmail.com>
2023-10-03 17:13:29 +0530

    updating folder structure

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
-
-
-import Login from "./components/Pages/login /Login";
-import Register from "./components/Pages/register /Register";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
 import { useContext } from "react";
-import {  Context } from "./context/ContextProvider";
-import Home from "./components/Pages/Home/Home";
-import About from "./components/Pages/about/About";
-// import Chats from "./components/chats/Chats";
-import Main from './components/Pages/main/Main'
-
-
+import { Context } from "./context/ContextProvider";
 
 function App() {
-  const {user} = useContext( Context);
+  const { user } = useContext(Context);
   console.log(user);
 
   return (
     <>
- 
-
-   <Router>
-     <Routes> 
-       
-     <Route exact path="/" element={user?<Main/>:<Login/>}/>
-     <Route exact path="/login" element={<Login/>}/>
-     <Route exact path="/register" element={<Register/>}/> 
-     <Route exact path="/chat" element={user?<Home/>:<Login/>}/> 
-     <Route exact path="/about" element={user?<About/>:<Register/>}/> 
-  
-    </Routes>
-   </Router>
-     </>
-    
+      <h1>hello</h1>
+    </>
   );
 }
 

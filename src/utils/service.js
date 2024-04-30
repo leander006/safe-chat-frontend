753df7433a4ccd68e913038c253dfdff9c8d285b
leander006 <leanderdsilva06@gmail.com>
2024-04-30 09:48:51 +0530

    updatiing base url

--- src/utils/service.js
+++ src/utils/service.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://safe-chat-1usy.onrender.com";
-export const BASE_URL = "http://localhost:3001";
+export const BASE_URL = "https://safe-chat-1usy.onrender.com";
+// export const BASE_URL = "http://localhost:3001";
